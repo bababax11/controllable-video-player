e19ade7d330f1979bb8f1dd3993c0e6a57d952f6
Odagawa <xtctl.1234@gmail.com>
2019-11-20 17:00:28 +0900

    message controll

---
+++ player/keycodes.h
@@ -0,0 +1,6 @@
+#ifndef KEYCODES_H_
+#define KEYCODES_H_
+constexpr int WAITKEY_CURSORKEY_RIGHT = 83;
+constexpr int WAITKEY_CURSORKEY_LEFT = 81;
+
+#endif
--- player/main.cpp
+++ player/main.cpp
@@ -1,4 +1,6 @@
 #include <opencv4/opencv2/opencv.hpp>
+#include <iostream>
+#include "keycodes.h"
 
 int main(int argc, char const *argv[])
 {
@@ -11,15 +13,20 @@ int main(int argc, char const *argv[])
     
     cv::VideoCapture cap(argv[1]);
     const int len_frames = int(cap.get(cv::CAP_PROP_FRAME_COUNT));
-    int frame_n = len_frames;
+    int frame_n = 1;
+    cap.set(cv::CAP_PROP_POS_FRAMES, frame_n);
+
     while(cap.read(frame)) {
         cv::imshow("Playing", frame);
         const int key = cv::waitKey(1);
-        if(key == 'q'/*113*/)//qボタンが押されたとき
+        if(key == int('q')/*113*/)//qボタンが押されたとき
         {
             break;//whileループから抜ける．
+        } else if (key == WAITKEY_CURSORKEY_RIGHT/*right arrow*/) {
+            frame_n +=1;
+        } else if (key == WAITKEY_CURSORKEY_LEFT/*left arrow*/) {
+            frame_n -=1;
         }
-        frame_n -=1;
         cap.set(cv::CAP_PROP_POS_FRAMES, frame_n);
     }
     cv::destroyAllWindows();
